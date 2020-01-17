b35e4a56fa3525b3ed151c63fa9f7bb940032f10
Youngju <yj8638@gamil.com>
2020-01-18 01:59:33 +0900

    ~14강

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Item # 현재 디렉토리의 models에서 Item import 한다고
 
-# Register your models here.
+admin.site.register(Item)
+list_display = ['pk', 'name', 'short_desc', 'price', 'is_publish']
+list_display_links = ['name']
+list_filter = ['is_publish']
+search_fields = ['name', 'updated_at']
+def short_desc(self, item):
+    return item.desc[:20]
+
+
